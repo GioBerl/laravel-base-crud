815730d6739431fd35b439e8627dd39cb5e993bd
Gio <giovanni.berl1@gmail.com>
2020-07-09 17:41:41 +0200

    first commit ora inizia a gestire le rotte

---
+++ database/seeds/StudentTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Student;
+
+
+class StudentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10; $i++) { 
+            $new_student = new Student();
+            $new_student->firstname = $faker->word;
+            $new_student->lastname = $faker->word;
+            $new_student->sr_num = $faker->randomNumber(5, true);
+            $new_student->email = $faker->email;
+            $new_student->save();
+        }
+    }
+}
