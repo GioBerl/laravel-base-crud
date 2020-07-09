0dec7d22fc13c5dbdfa01a30366ce673d68d00a9
Gio <giovanni.berl1@gmail.com>
2020-07-09 23:05:35 +0200

    ok

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -7,22 +7,30 @@
     <div class="row">
         <div class="col-12">
             <h1 class="mt-3 mb-3">Crea un nuovo Studente</h1>
-            <form>
+            <form action="{{route('students.store')}}" method="post">
+                @csrf
+                @method('POST')
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                        placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                        else.</small>
+                    <label for="firstname">Firstname</label>
+                    <input type="text" name="firstname" class="form-control" id="firstname"
+                        placeholder="Inserisci Nome">
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <label for="lastname">Lastname</label>
+                    <input type="text" name="lastname" class="form-control" id="lastname"
+                        placeholder="Inserisci Cognome">
                 </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="form-group">
+                    <label for="sr_num">Serial Number</label>
+                    <input type="number" name="sr_num" class="form-control" id="sr_num"
+                        placeholder="Inserisci Matricola">
                 </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Inserisci Email">
+                </div>
+
+
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
